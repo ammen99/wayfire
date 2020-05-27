e39974b0a4af77b83240e775741cc416066c93aa
Damian Ivanov <damianatorrpm@gmail.com>
2020-05-28 02:17:26 +0300

    I like the other coding style more though!

--- src/output/gtk-shell.hpp
+++ src/output/gtk-shell.hpp
@@ -10,4 +10,4 @@ struct wl_resource;
 wf_gtk_shell *wf_gtk_shell_create(wl_display *display);
 
 std::string wf_gtk_shell_get_custom_app_id(wf_gtk_shell *shell,
-                                           wl_resource *surface);
+    wl_resource *surface);
