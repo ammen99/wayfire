f1fa88cae49f1d01cd7172b371fcff4cbdb0e1f7
Ilia Bozhinov <ammen99@gmail.com>
2020-07-26 15:13:46 +0300

    Update src/output/gtk-shell.cpp

--- src/output/gtk-shell.cpp
+++ src/output/gtk-shell.cpp
@@ -102,7 +102,7 @@ static void handle_gtk_surface_request_focus(struct wl_client *client,
     wayfire_view view = wf::wl_surface_to_wayfire_view(surface);
     if (view)
     {
-        wf::self_request_focus_signal data;
+        wf::view_self_request_focus_signal data;
         data.view = view;
         view->get_output()->emit_signal("self-request-focus", &data);
     }
