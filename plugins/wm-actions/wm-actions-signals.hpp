16eefe63ecede534b98a5488a031fea1366d5771
Damian Ivanov <damianatorrpm@gmail.com>
2020-07-29 15:05:18 +0300

    wm-actions-signal
    signal names
    
    to squash later
    
    same
    
    right

---
+++ plugins/wm-actions/wm-actions-signals.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <wayfire/signal-definitions.hpp>
+
+/**
+ * name: wm-actions-toggle-above
+ * on: output
+ * when: Emitted whenever some entity requests that the view's above state
+ *   is supposed to change.
+ */
+using wm_actions_toggle_above = wf::_view_signal;
+
+/**
+ * name: wm-actions-toggle-above-changed
+ * on: output
+ * when: Emitted whenever a views above layer has been changed.
+ */
+using wm_actions_above_changed = wf::_view_signal;
