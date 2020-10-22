ef4a8824f656ca35b48d43e5827112ba86a8b570
trigg <triggerhapp@googlemail.com>
2020-10-22 15:17:37 +0000

    Removed unneeded assignments

--- plugins/common/wayfire/plugins/common/cairo-util.hpp
+++ plugins/common/wayfire/plugins/common/cairo-util.hpp
@@ -30,9 +30,7 @@ static void cairo_surface_upload_to_texture(
     GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
     GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
     GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_BLUE));
-    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_GREEN));
     GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_RED));
-    GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ALPHA));
     GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
         buffer.width, buffer.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, src));
 }
